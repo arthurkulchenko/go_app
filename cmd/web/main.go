c478ec7e449a357c9a31226a7034a892a7cb072e
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2022-09-11 18:54:15 +0300

    Sectino 3, lesson 28: split functionality into different modules

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/arthurkulchenko/go_app/pkg/handlers"
 	// "html/template"
 )
 
 const PORT_NUMBER = ":8080"
 
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("=======================\nStarting application on\nlocalhost%s\n=======================", PORT_NUMBER))
 	_ = http.ListenAndServe(PORT_NUMBER, nil)
