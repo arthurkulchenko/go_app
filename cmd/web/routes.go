a9519aedbd075299d4873a0c54cf7f6ff4a97a85
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2022-09-16 01:27:36 +0300

    add middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,15 +1,17 @@
 package main
 
 import(
-	"net/http"
 	"github.com/arthurkulchenko/go_app/pkg/config"
 	"github.com/arthurkulchenko/go_app/pkg/handlers"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
 )
 
 func Routes(appP *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
 	mux.Get("/", handlers.RepositoryPointer.Home)
 	mux.Get("/about", handlers.RepositoryPointer.About)
 	return mux
